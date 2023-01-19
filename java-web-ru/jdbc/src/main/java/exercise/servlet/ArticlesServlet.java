9e3e2c4a265f971b73a2cd26908aba3b5914b6b1
@hexlet/cli <support@hexlet.io>
2023-01-19 21:30:39 +0600

    submit java-web-ru/jdbc

--- java-web-ru/jdbc/src/main/java/exercise/servlet/ArticlesServlet.java
+++ java-web-ru/jdbc/src/main/java/exercise/servlet/ArticlesServlet.java
@@ -7,8 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletContext;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import org.apache.commons.lang3.ArrayUtils;
 
